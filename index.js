c9dc9a4ef8bd74d59acbb4a803fd7e9df5e24ad5
cptblackmore <redshatterr@gmail.com>
2025-01-18 14:09:47 +0300

    Made authentication logic

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const express = require('express');
 const cors = require('cors');
+const router = require('./lib/router');
+const errorMiddleware = require('./lib/middleware/error-middleware');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 const app = express();
 
 app.use(express.json());
@@ -13,15 +15,15 @@ app.use(cors({
   origin: process.env.CLIENT_URL
 }));
 app.use(cookieParser());
-// app.use('/api', router);
-// app.use(errorMiddleware);
+app.use('/api', router);
+app.use(errorMiddleware);
 
 const start = async () => {
   try {
     await mongoose.connect(process.env.DB_URL, {
       tls: true
-    }).then(() => console.log('DB connected'));
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    }).then(() => console.log('\x1b[32m%s\x1b[1m', 'DB connected'));
+    app.listen(PORT, () => console.log('\x1b[32m%s\x1b[1m', `Server started on port ${PORT}`));
   } catch (e) {
     console.log(e);
   }

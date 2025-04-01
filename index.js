5ebd373aa6570153e0ab045b8a463b3cff331559
cptblackmore <redshatterr@gmail.com>
2025-04-01 15:44:48 +0300

    Added second env var usage for local development.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(refererMiddleware);
 app.use(cors({
   credentials: true,
-  origin: process.env.CLIENT_URL
+  origin: [process.env.CLIENT_URL, process.env.CLIENT_DEV_URL]
 }));
 app.use(cookieParser());
 app.use('/api', router);
